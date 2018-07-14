534d0156970ffa0e62240ea88e40b20d07ad4f4a
FiNkacho <sabaxuxunashvili@gmail.com>
2018-07-15 00:31:07 +0400

    Added socket connection

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.render('index.ejs');
+});
+
+io.on('connection', function(socket){
+  console.log('User connected');
+  socket.on('url-change', function(data){
+    // TODO
+    // 1. get room by room id ( data['id'] )
+    // 2. set value of video id to data['video-id']
+    console.log(data);
+  });
+
+  socket.on('join', function(id){
+    // TODO
+    // 1. get room by room id
+    // 2. get video id
+    console.log("Joined Room: "+id);
+  });
+
 });
 
 http.listen(8080, function(){
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,39 @@
 const youtubeLink = document.getElementById('youtube-link');
 const submitBtn = document.getElementById('link-submit');
+const generate = document.getElementById('generate');
+const join = document.getElementById('join');
+const roomId = document.getElementById('room-id');
+const socket = io();
+
+// Generates random id
+var ID = function () {
+  return Math.random().toString(36).substr(2, 9);
+};
+
+// initial value
+var id = ID();
+roomId.value = id;
 
 submitBtn.addEventListener("click", function(){
   var link = youtubeLink.value;
-  var id = link.split("v=")[1].split('?')[0];
-  player.loadVideoById(id);
+  var videoId = link.split("v=")[1].split('?')[0];
+  urlChange(id, videoId);
+});
+
+generate.addEventListener("click", function(){
+  id = ID();
+  roomId.value = id;
+  urlChange(id, videoId);
+});
+
+join.addEventListener("click", function(){
+  id = roomId.value;
+  socket.emit('join', id);
 })
+
+function changeUrl(id, videoId){
+  socket.emit('url-change', {
+    'id': id,
+    'video-id': player.getVideoData()['video_id']
+  });
+}
